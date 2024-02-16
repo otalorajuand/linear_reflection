b60100b78724f003df48c5a4c192eab0365d0238
Otalorajuand <otalorajuand@gmail.com>
2024-02-16 11:22:16 -0500

    Improvement of class line

--- main.py
+++ main.py
@@ -10,32 +10,53 @@ m2 = -0.181818181818182
 b2 = 8.18181818181818
 
 class Line():
+
+    global x
+    x = symbols('x', real=True)
   
-    def __init__(self, m, b):
+    def __init__(self, m=None, b=0):
         self.m = m
         self.b = b
 
-    def reflection_line(self, wall):
-       
-        x = symbols('x', real=True)
-        
-        tan = abs((self.m - wall.m)/(1 + self.m*wall.m))
+    def reflection_slope(self, wall):
+
+        if self.m == None:
+            tan = tan = abs(1/wall.m)
+        else:
+            tan = abs((self.m - wall.m)/(1 + self.m*wall.m))
+
         expr = tan - abs(x - wall.m)/abs(1 + x*wall.m)
         sol = solve(expr, x)
 
-        m3 = sol[1] if sol[0] == m2 else sol[0]
+        m = sol[1] if len(sol) > 1 else sol[0]
 
-        expr = (self.m*x + self.b) - (wall.m*x + wall.b)
-        sol = solve(expr, x)
+        return m
+    
+    def reflection_intercept(self, wall, reflection_slope):
 
-        y1 = wall.m*sol[0] + wall.b
-        b3 = y1 - m3*sol[0]
+        if self.m == None:
 
-        print(f"The slope of the new line is: {m3}")
-        print(f"The intercept with the Y axis is: {b3}")
-        print(f"The equation of the new line is: y = {m3}*x + {b3}")
+            return 1 / (4*0.1)
+        
+        else:
+            
+            expr = (self.m*x + self.b) - (wall.m*x + wall.b)
+            sol = solve(expr, x)
 
-        return Line(m3, b3)
+            y1 = wall.m*sol[0] + wall.b
+            return y1 - reflection_slope*sol[0]
+
+
+    def reflection_line(self, wall):
+
+        m = self.reflection_slope(wall)
+        b = self.reflection_intercept(wall, m)
+
+        print(f"The slope of the new line is: {m}")
+        print(f"The intercept with the Y axis is: {b}")
+        print(f"The equation of the new line is: y = {m}*x + {b}")
+
+        return Line(m, b)
 
 
 original_line = Line(m2, b2)
